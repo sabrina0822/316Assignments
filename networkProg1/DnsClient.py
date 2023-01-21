1e3afcf6ad76d40d1ffbd03187010ec4e351ae9e
Mathieu Geoffroy <mathieu.geoffroy@mail.mcgill.ca>
2023-01-21 13:05:24 -0500

    add positional args and basic address validation

--- networkProg1/DnsClient.py
+++ networkProg1/DnsClient.py
@@ -1,5 +1,6 @@
 import socket 
 import argparse
+from re import match
 
 '''
 python DnsClient [-t timeout] [-r max-retries] [-p port] [-mx | nx] @server name
@@ -11,14 +12,28 @@ python DnsClient [-t timeout] [-r max-retries] [-p port] [-mx | nx] @server name
 -ns (IP address): send a name server query 
 
 '''
+
+def valid_server_address(server_address):
+    try:
+        if match(r'@*.*.*.*', server_address): # TODO add better regex for valid IP address if wanted
+            return server_address
+        else:
+            raise ValueError
+    except:
+        msg = "not a valid server address: {0!r}".format(server_address)
+        raise argparse.ArgumentTypeError(msg)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', type=float, default=5.0)
 parser.add_argument('-r', type=int, default=3)
 parser.add_argument('-p', type=int, default=53)
-
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-ns', action='store_true', default=False)
 group.add_argument('-mx', action='store_true', default=False) 
+parser.add_argument('server', type=valid_server_address)
+parser.add_argument('name', type=str)
+
+
 
 
 args = parser.parse_args()
@@ -30,4 +45,4 @@ port = args.p
 mail_server = args.mx
 name_server = args.ns
 
-print(f"timeout {timeout}\nretries {retries}\nport {port}\nmail_server {mail_server}\nname_server {name_server}")
+print(f"timeout {timeout}\nretries {retries}\nport {port}\nmail_server {mail_server}\nname_server {name_server}\nserver {args.server}\nname {args.name}")
