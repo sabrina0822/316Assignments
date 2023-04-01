ad5814f11e5c0f96a76bec99f0fe2a70a8a2f8de
sabrina0822 <smansour@DESKTOP-U4JGKS8.localdomain>
2023-04-01 15:26:41 -0400

    inverse 1d fft now works

--- fastFourier2/fft.py
+++ fastFourier2/fft.py
@@ -1,6 +1,6 @@
 import argparse
 import numpy
-import matplotlib
+#simport matplotlib
 import cv2
 import math
 import cmath
@@ -178,32 +178,87 @@ def inverse_fft_1D(row_vector : numpy.ndarray):
 
     output_vector = numpy.zeros(N, dtype=complex)
 
-    # TODO Chose proper base case
-    # Base case
-    if N == 8:
-        return inverse_dft_1D(row_vector)
-    else:
-        # Split vector into even and odd indices
-        even = row_vector[0::2]
-        odd = row_vector[1::2]
+    tmpconj = numpy.conj(row_vector)
 
-        # Calculate FFT of even and odd indices
-        even_fft = inverse_fft_1D(even)
-        odd_fft = inverse_fft_1D(odd)
+    Xconj = fft_1D(tmpconj)
 
-        # Calculate the output vector
-        output_vector = numpy.zeros(N, dtype=complex)
-        for k in range(N // 2):
-            output_vector[k] = even_fft[k] + odd_fft[k] * cmath.exp(1j * 2 * math.pi * k / N)
-            output_vector[k + N//2] = even_fft[k] - odd_fft[k] *  cmath.exp(1j * 2 * math.pi * k / N)
+    output_vector = Xconj / N
 
-        return output_vector
 
-# def inverse_fft_2D():
+    # # TODO Chose proper base case
+    # # Base case
+    # if N == 8:
+    #     return inverse_dft_1D(row_vector)
+    # else:
+    #     # Split vector into even and odd indices
+    #     even = row_vector[0::2]
+    #     odd = row_vector[1::2]
 
-# def plot_dft(): 
+    #     # Calculate FFT of even and odd indices
+    #     even_fft = inverse_fft_1D(even)
+    #     odd_fft = inverse_fft_1D(odd)
+
+    #     # Calculate the output vector
+    #     output_vector = numpy.zeros(N, dtype=complex)
+    #     for k in range(N // 2):
+    #         output_vector[k] = even_fft[k] + odd_fft[k] * cmath.exp(1j * 2 * math.pi * k / N)
+    #         output_vector[k + N//2] = even_fft[k] - odd_fft[k] *  cmath.exp(1j * 2 * math.pi * k / N)
+
+
+    return output_vector
+
+def inverse_fft_2D():
+    """
+    Calculate the FFT of the given 2D input signal
+    Input: a 2D array of values
+    """ 
+    if __debug__:
+        print(f"Inverse FFT 2D - Input Matrix:\n{matrix}\n\n")
+
+    # N rows, M columns
+    N, M = matrix.shape
+
+    # Make matrix size to nearest power of 2
+    pow2N = math.ceil(math.log2(N))
+    N = 2 ** pow2N
+
+    pow2M = math.ceil(math.log2(M))
+    M = 2 ** pow2M
+
+    # Pad matrix with zeros
+    matrix = numpy.pad(matrix, ((0, N - matrix.shape[0]), (0, M - matrix.shape[1])), 'constant')
+
+    output_matrix = numpy.zeros((N, M), dtype=complex)
+    inner_matrix = numpy.zeros((N, M), dtype=complex)
+        
+    if __debug__:
+        print("Row: ", flush=True, end="")
+
+    for n in range(N):
+        if __debug__:
+            print(f"{n} ", flush=True, end=" ")
+        inner_matrix[n, 0:M] = inverse_fft_1D(matrix[n, 0:M])
+
+    if __debug__:
+        print(f"\n\nCol: ", flush=True, end="")
+
+    for m in range(M):
+        if __debug__:
+            print(f"{m}", flush=True, end=" ")
+        output_matrix[0:N, m] = inverse_fft_1D(inner_matrix[0:N, m])
+
+    if __debug__:
+        print(f"\n\nFFT 2D - Output Matrix:\n{output_matrix}\n\n")
+        
+    return output_matrix
+
+def plot_dft():
+    """
+    Plots the resulting output DFT on a log scale plot
+    """
 
 # def save_dft():
+
 
 
 def collect_args():
@@ -240,8 +295,57 @@ if __name__ == "__main__":
     vector2 = numpy.pad(vector, (0, 1024 - len(vector)), 'constant')
     print(vector)
     print("\n\n")
-    print(inverse_dft_1D(dft_1D(vector2))*1/1024)
+    print(numpy.real(inverse_dft_1D(dft_1D(vector2))*1/1024))
     print("\n\n\n")
-    print(inverse_fft_1D(fft_1D(vector))*1/1024)
+    print(numpy.real(inverse_fft_1D(fft_1D(vector))))
 
     # print(f"Oracle:\n{oracle}\n\n")
+
+"""
+Notes for sabrina: 
+Mode 1: 
+    Perform FFT 
+    Output a one by two subplot of the original image 
+    besides the subplot output its fourier transform 
+
+    Fourier transform needs to be log scaled --> LogNorm from matplotlib.colours
+    Produces a logarithmic colour map???
+
+
+Mode 2: 
+    Output a 1x2 subplot of original image next to its denoised version
+
+    denoised version: 
+    take the FFt of the image and set all high frequencies to zero before 
+    inverting back to the filtered original 
+
+    "high" frequencies can be determined from trial and error 
+
+    FFT is from 0 to 2pi --> any frequency close to 0 or 2 pi is considered low 
+
+    Program should also print the number of non-zeroes using and the fraction 
+    they represent of th eoriginal Fourier coefficients 
+
+Mode 3: 
+    Take the FFT of the image to compress it 
+    Compression comes from setting some fourier coefficients to 0 
+        1.can threshold the coefficient's magnitude and take only the largest percentile of them?????
+        2.keep all the coefficients of very low frequencies as well as a fraction of the largest coefficients 
+        from higher frequencies to also filter the image at the same time 
+
+        Experiment with various schemes and decide what works best and justify it in the report 
+    
+    Then 
+    Display a 2x3 subplot of the image at 6 different compression levels 
+        start from original image (0%) 
+        to setting 95% of the coefficients to 0 
+
+    Get the images by inverse transofrming the modified Fourier coefficients 
+
+    Save the Fourier transform matrix of coefficients in a csv, txt or soemthing else 
+    Program should print the number of nonszero Fourier coefficients in each of the 6 images 
+
+Mode 4: 
+    Produce plots that summarize the runtime complexity of your algorithms 
+    Code should print the means and variances of the runtime of your algorithms vs the problem size
+"""
